b9dcfa1c00a8b2906f96544110288a4e8e17b5cc
gipobbe <56393691+gipobbe@users.noreply.github.com>
2021-09-04 10:51:43 +0200

    add flurl to project add http client

---
+++ Core/AppHttpClient/AppHttpClientRequestOptions.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace weather_app.Core.AppHttpClient
+{
+    public class AppHttpClientRequestOptions
+    {
+        public Dictionary<string, string>? Headers { get; set; }
+    }
+}
